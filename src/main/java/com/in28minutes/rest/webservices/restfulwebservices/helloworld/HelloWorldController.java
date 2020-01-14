799e74b9d1b1fbeb68fbd3e0621597029e24443b
raghu694 <04.raghu1@gmail.com>
2020-01-14 18:06:55 +0530

    raghu5

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -10,12 +10,12 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World v2";
+		return "Hello World v3";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
-		return new HelloWorldBean("Hello World--------------------->");
+		return new HelloWorldBean("Hello World---------------------------->");
 	}
 	
 	///hello-world/path-variable/in28minutes
